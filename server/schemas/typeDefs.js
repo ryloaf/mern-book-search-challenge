4d1ddae4e6ee2020d10f66d55104456d7113bb93
Rylee Jolley <you@example.com>
2024-06-05 11:52:07 -0600

    made changes to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,46 +1,43 @@
 // define query and mutation types
-const { gql } = require("apollo-server-express");
+// const { gql } = require("apollo-server-express");
 
-const typeDefs = gql`
-    type User {
-        _id: ID
-        username: String
-        email: String
-        password: String
-        bookCount: Int
-        savedBooks: [Book]
-    }
-    type Book {
-        bookId: ID
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
-    type Query {
-        me: User
-        users: [User]
-        user(username: String!): User
-    }
-    input BookInput {
-        authors: [String]
-        description: String
-        bookId: String!
-        image: String
-        link: String
-        title: String!
-    }
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(bookInput: BookInput): User
-        removeBook(bookId: ID!): User
-    }
-    type Auth {
-        token: ID!
-        user: User
-    }
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String!
+  }
+  type Query {
+    me: User
+  }
+  input SearchBooks {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: SearchBooks!): User
+    removeBook(bookId: ID!): User
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
