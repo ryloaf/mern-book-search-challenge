5c3fd315b74d169362afc83de1e6bfbf57cfad02
Rylee Jolley <you@example.com>
2024-06-05 11:06:07 -0600

    updated connection.js and auth.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+//mongoDB connnection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: true,
+});
 
 module.exports = mongoose.connection;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,6 +5,17 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+
+  // signToken: function ({ username, email, _id }) {
+  //   const payload = {username, email, _id };
+  //   return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  // },
+
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
