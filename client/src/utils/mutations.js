899eacafee2cc9b263b3a67744a7031013e0e7c6
Rylee Jolley <you@example.com>
2024-06-05 12:05:08 -0600

    made changes to mutations, quieries and dependencies

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,58 +3,62 @@ import { gql } from '@apollo/client';
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-    token
-    user {
-        _id
-        username
-    }
+      _id
+      username
+      email
+      password
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
   }
 `;
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUSer(username: $username, email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-      }
-    }
-  }
-`;
-
-export const SAVE_BOOK = gql`
-mutation saveBook($bookInput: BookInput!) {
-    saveBook(bookInput: $bookInput) {
+    addUser(username: $username, email: $email, password: $password) {
       _id
       username
       email
-      savedBooks {
-        bookId
-        authors
-        image
-        description
-        title
-        link
-      }
-    }
-  }
-`;
-export const REMOVE_BOOK = gql`
-mutation removeBook($bookId: ID!) {
-    removeBook(bookId: $bookId) {
-      _id
-      username
-      email
+      password
       bookCount
       savedBooks {
         bookId
         authors
+        description
+        title
         image
         link
-        title
-        description
       }
+    }
+  }
+`;
+export const SAVE_BOOK = gql`
+  mutation saveBook($criteria: SearchBooks) {
+    saveBook(criteria: $criteria) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+`;
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
     }
   }
 `;
